aea47d06bb09fa32389eb78cdd0c12ed64a5b978
Theeastsea <905762202@qq.com>
2018-10-27 14:21:30 +0800

    2018.10.17

--- AcFun/MainPage.xaml.cs
+++ AcFun/MainPage.xaml.cs
@@ -12,46 +12,47 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using System.Threading.Tasks;
 
 // https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x804 上介绍了“空白页”项模板
 
 namespace AcFun
 {
+
     /// <summary>
     /// 可用于自身或导航至 Frame 内部的空白页。
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        public static string ACFUN_url = "http://www.acfun.cn";
+
+  
         public MainPage()
         {
+           
+            
             this.InitializeComponent();
-            GetContent.ShouYeAsync();//可以放异步方法 在里面完成异步操作
+
+            FlipView();//没有返回值的异步方法就不需要等待(await)
+
+
+
+
+
 
         }
 
+        public async void FlipView()//显示flip的内容 没有返回值的异步方法就不需要等待(await)
+        {
+            DateInfoInFrist info = new DateInfoInFrist();
+           
+            var list =await  info.GetShouYeInfo();
 
+            Flip.ItemsSource = list.FindAll((d) => d.i == 1);//检索的另一种格式List<Entity> ltEntity= entCollection.FindAll(delegate(Entity m) { return m.name== "aa"; })
 
+            gridview.ItemsSource= list.FindAll((d) => d.i == 2);
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        }
 
 
 
